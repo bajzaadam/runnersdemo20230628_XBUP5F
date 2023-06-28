d20ab33b9784c21caa47d68ab1278f627f9ac487
bajzaadam <134214443+bajzaadam@users.noreply.github.com>
2023-06-28 16:35:38 +0200

    átlagiram számítása és kiirása

--- src/main/java/hu/gde/runnersdemo/RunnerService.java
+++ src/main/java/hu/gde/runnersdemo/RunnerService.java
@@ -34,4 +34,13 @@ public class RunnerService {
         List<RunnerEntity> runners = runnerRepository.findAll();
         return Collections.max(runners, Comparator.comparing(runner -> runner.getShoeSize()));
     }
+    public double getAveragePaceTime()
+    {
+        List<RunnerEntity> runners = runnerRepository.findAll();
+        double sumPace = 0;
+        for(RunnerEntity runner : runners) {
+            sumPace += runner.getPace();
+        }
+        return sumPace / runners.size();
+    }
 }
